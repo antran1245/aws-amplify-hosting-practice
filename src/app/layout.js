b4de58676e4e3194fe4cbc2d2ebacf6dcce2c45c
An <antran1245@gmail.com>
2024-05-09 16:49:07 -0700

    pass og image to public folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,27 +6,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Hello world!",
-  description: "Simple hosting test for password protected website.",
-  openGraph: {
-    title: "Hello World! My name is An",
-    description: "Look at my message to the world.",
-    url: 'https://main.d3tqd4h05q91nm.amplifyapp.com/',
-    type: 'website',
-    images: [
-      {
-        width: 1200,
-        height: 900,
-        alt: 'Pie picture'
-      }
-    ]
-  }
+  description: "Simple hosting test for password protected website."
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta property="og:image" content="./opengraph-image.jpeg" />
+        <meta property="og:title" content="Hello World! My name is An" />
+        <meta property="og:description" content="Look at my message to the world." />
+        <meta property="og:url" content="https://main.d3tqd4h05q91nm.amplifyapp.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/opengraph-image.jpeg" />
+        <meta property="og:image:alt" content="Pie Picture" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
