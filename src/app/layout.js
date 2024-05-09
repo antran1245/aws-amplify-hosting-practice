dbd3e9c42b350d2ed7bdbc22efeb8dcee5fa3f10
An <antran1245@gmail.com>
2024-05-08 19:53:58 -0700

    metadata for image for social media preview

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,16 @@ export const metadata = {
   openGraph: {
     title: "Hello World! My name is An",
     description: "Look at my message to the world.",
-    images: "https://images.unsplash.com/photo-1584468032442-d22740512124?q=80&w=1981&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    url: 'https://main.d3tqd4h05q91nm.amplifyapp.com/',
+    type: 'website',
+    images: [
+      {
+        url: "https://images.unsplash.com/photo-1584468032442-d22740512124?q=80&w=1981&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        width: 1200,
+        height: 900,
+        alt: 'Pie picture'
+      }
+    ]
   }
 };
 
