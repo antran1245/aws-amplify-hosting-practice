1c955b6855dc8b780364ad6029032eea99f42bb6
An <antran1245@gmail.com>
2024-05-09 16:52:06 -0700

    move the Head meta to page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,16 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta property="og:title" content="Hello World! My name is An" />
-        <meta property="og:description" content="Look at my message to the world." />
-        <meta property="og:url" content="https://main.d3tqd4h05q91nm.amplifyapp.com/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="/opengraph-image.jpeg" />
-        <meta property="og:image:alt" content="Pie Picture" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,16 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Head>
+        <meta property="og:title" content="Hello World! My name is An" />
+        <meta property="og:description" content="Look at my message to the world." />
+        <meta property="og:url" content="https://main.d3tqd4h05q91nm.amplifyapp.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/opengraph-image.jpeg" />
+        <meta property="og:image:alt" content="Pie Picture" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
       <Sections>Hello</Sections>
       <Sections>World!</Sections>
       <Sections>My</Sections>
