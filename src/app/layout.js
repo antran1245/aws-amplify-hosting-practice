3e4a5f7aaec8d54f7857769cc0f1038b80334d5d
An <antran1245@gmail.com>
2024-05-09 18:42:50 -0700

    added some more og meta tags and comments

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  metadataBase: new URL('https://main.d3tqd4h05q91nm.amplifyapp.com/'),
+  metadataBase: new URL('https://main.d3tqd4h05q91nm.amplifyapp.com/'), // Base URL
   title: "Hello world!",
   description: "Simple hosting test for password protected website."
 };
@@ -13,10 +13,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        {/* Generate the open graph image by linking to the opengraph-image.(jpg|jpeg|png|gif) */}
         <meta property="og:image" content="<generated>" />
         <meta property="og:image:type" content="<generated>" />
         <meta property="og:image:width" content="<generated>" />
         <meta property="og:image:height" content="<generated>" />
+        <meta property="og:url" content="https://main.d3tqd4h05q91nm.amplifyapp.com/" />
+        <meta property="og:type" content="website" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
