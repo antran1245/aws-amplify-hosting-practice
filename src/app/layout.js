418f963580ec34b5c2c5797513da9e1800aa05f6
An <antran1245@gmail.com>
2024-05-09 17:18:28 -0700

    different approach to opengraph image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Hello world!",
   description: "Simple hosting test for password protected website."
 };
+const openGraphImage = "/opengraph-image.jpeg"
 
 export default function RootLayout({ children }) {
   return (
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
         <meta property="og:description" content="Look at my message to the world." />
         <meta property="og:url" content="https://main.d3tqd4h05q91nm.amplifyapp.com/" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/opengraph-image.jpeg" />
+        <meta property="og:image" content={openGraphImage} />
         <meta property="og:image:alt" content="Pie Picture" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
