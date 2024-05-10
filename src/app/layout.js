cdc3dd424cc992bb22b49f7ed83a9f226e0e8970
An <antran1245@gmail.com>
2024-05-09 17:34:33 -0700

    try metadataBase

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,23 +4,19 @@ import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
+  metadataBase: new URL('https://main.d3tqd4h05q91nm.amplifyapp.com/'),
   title: "Hello world!",
   description: "Simple hosting test for password protected website."
 };
-const openGraphImage = "/opengraph-image.jpeg"
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <meta property="og:title" content="Hello World! My name is An" />
-        <meta property="og:description" content="Look at my message to the world." />
-        <meta property="og:url" content="https://main.d3tqd4h05q91nm.amplifyapp.com/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content={openGraphImage} />
-        <meta property="og:image:alt" content="Pie Picture" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
